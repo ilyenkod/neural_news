ecd54c0367e60c08b2510f4c4b102f78bc8f0067
ilyenkod <ilyenkod@yandex.ru>
2025-08-04 23:36:20 +0700

    add setup files

---
+++ components/web_service/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="web-service",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    python_requires=">=3.12",
+    author="Dmitry Ilyenko",
+    author_email="ilyenkod@yandex.ru",
+    description="Web service for Neural News Platform",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
